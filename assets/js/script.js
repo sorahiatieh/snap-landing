bb22f86cff07906a63aa882e106e248c4e841041
atieh <a.sorahitalab@gmail.com>
2026-07-25 20:10:32 +0330

    added slider section

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+    //********************* OWL CAROUSEL ****************************
+    
+    $(function () {
+        $(".owl-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true
+        });
+    });
+
